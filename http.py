75b4321ce54491319679dada6ba9d41d5fad4431
matman26 <a.matheus26@hotmail.com>
2018-11-29 15:29:42 +0100

    added send_request function to loris

--- http.py
+++ http.py
@@ -12,9 +12,7 @@ SYN = ip/TCP(sport=port, dport=80, flags="S", seq=42)
 
 # Send SYN and wait for SYNACK
 SYNACK = sr1(SYN)
-SYNACK.show()
 
 # Send ACK and GET
 ACKGET=ip/TCP(sport=SYNACK.dport, dport=80, flags="A", seq=SYNACK.ack, ack=SYNACK.seq + 1)/get
-reply,error=sr(ACKGET)
-reply.show()
+send(ACKGET)
--- loris.py
+++ loris.py
@@ -1,5 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Imports
-import scapy.all as scapy
+from scapy.all import *
 
+def send_get(ip_addr):
+    ip = IP(dst=ip_addr)
+    get='GET / HTTP/1.0\n\n'
+    port = RandNum(1024,6500)
+    SYN = ip/TCP(sport=port, dport=80, flags="S", seq=42)
+
+    # Send SYN and wait for SYNACK
+    SYNACK = sr1(SYN)
+
+    # Send ACK and GET
+    ACKGET=ip/TCP(sport=SYNACK.dport, dport=80, flags="A", seq=SYNACK.ack, ack=SYNACK.seq + 1)/get
+
+    send(ACKGET)
+
+def main():
+
+    # Make requests
+    send_get("35.204.58.85")
+
+
+
+
+if __name__== "__main__":
+    main()
