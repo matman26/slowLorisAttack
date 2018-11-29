a9d05e45dab40ab655bf22fe13db680b0d0332f6
matman26 <a.matheus26@hotmail.com>
2018-11-29 14:41:27 +0100

    http.py working

--- http.py
+++ http.py
@@ -2,14 +2,19 @@
 
 # Imports
 from scapy.all import *
+import time
 
-# Make a packet
-ip = IP(dst="www.google.com")
-port = 1235
+# Make a TCP SYN
+ip = IP(dst="35.204.58.85")
+get='GET / HTTP/1.0\n\n'
+port = RandNum(1024,6500)
 SYN = ip/TCP(sport=port, dport=80, flags="S", seq=42)
-SYN.show()
 
-# Send it and wait for response
+# Send SYN and wait for SYNACK
 SYNACK = sr1(SYN)
 SYNACK.show()
-# show message we received
+
+# Send ACK and GET
+ACKGET=ip/TCP(sport=SYNACK.dport, dport=80, flags="A", seq=SYNACK.ack, ack=SYNACK.seq + 1)/get
+reply,error=sr(ACKGET)
+reply.show()
