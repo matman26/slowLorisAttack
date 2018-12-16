3785ce63d15fc3904c32d874cdf0377063abbf75
matman26 <a.matheus26@hotmail.com>
2018-12-16 14:14:46 +0100

    More  cleaning and some sections

--- http.py
+++ http.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
 # Imports
 from scapy.all import *
+from scapy.all import http
 
+# This handles HTTP responses from the server
+def handle(pkt):
+    if pkt.haslayer(http.HTTPrequest):
+        print(pkt)
+    
 # Make a TCP SYN
 #ip_address="35.204.58.85"    # Alessandro's server
 ip_address="192.168.33.10"    # Matheus' VM (see Vagrantfile)
-my_port=80                    # www port
+server_port=80                # http port
+ip = IP(dst=ip_address)       # IP Packet 
 
-ip = IP(dst=ip_address)
 # HTTP GET Request
-get = 'GET / HTTP/1.1\r\nHost: 192.168.33.10\r\n\r\n'
+get = "GET / HTTP/1.1\r\nHost: " + ip_address + "\r\n\r\n"
 port = RandNum(1024,6500)
-SYN = ip/TCP(sport=port, dport=my_port, flags="S", seq=42) 
 
-# Send SYN and wait for SYNACK
+# The TCP 3-way handshake rules must be followed. As such, we begin by sending a
+# TCP packet with the S (or SYN) flag set. The sequence number is irrelevant, as all
+# future TCP packets will contain sequence numbers which are increments of the number
+# given
+SYN = ip/TCP(sport=port, dport=server_port, flags="S", seq=42) 
+
+# Send SYN and wait for SYNACK as response
 print(SYN.summary())
 SYNACK = sr1(SYN)
 
-# Send ACK and GET
-ACK=ip/TCP(sport=SYNACK.dport, dport=my_port, flags="A", seq=SYNACK.ack, ack=(SYNACK.seq+1))/get
-data=sr1(ACK)
-data.show()
+# Send ACK and finally HTTP GET, sniff for the result
+ACK=ip/TCP(sport=SYNACK.dport, dport=server_port, flags="A", seq=SYNACK.ack, ack=(SYNACK.seq+1))/get
+sniff(filter="tcp and port 80", store=False, prn=handle)
 
